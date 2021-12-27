a91bde63da69f063134f7341493bf7eb3826fa84
Yao <yaol7@apac.dell.com>
2021-12-27 15:32:22 +0800

    fix88

--- src/main/java/com/github/mbode/flink_prometheus_example/PrometheusExampleJob.java
+++ src/main/java/com/github/mbode/flink_prometheus_example/PrometheusExampleJob.java
@@ -3,10 +3,13 @@ package com.github.mbode.flink_prometheus_example;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamUtils;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
 
 public class PrometheusExampleJob {
   private static final Logger log = LoggerFactory.getLogger(PrometheusExampleJob.class);
@@ -23,8 +26,8 @@ public class PrometheusExampleJob {
   private void run() throws Exception {
     log.info("metrics job is stating.");
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-    /*env.enableCheckpointing(500);
-    env.disableOperatorChaining();
+    //env.enableCheckpointing(500);
+    //env.disableOperatorChaining();
 
     env.addSource(new RandomSourceFunction(parameters.getInt("elements", Integer.MAX_VALUE)))
         .name(RandomSourceFunction.class.getSimpleName())
@@ -32,11 +35,8 @@ public class PrometheusExampleJob {
         .name(FlinkMetricsExposingMapFunction.class.getSimpleName())
         .addSink(new DiscardingSink<>())
         .name(DiscardingSink.class.getSimpleName());
-    //env.execute(PrometheusExampleJob.class.getSimpleName());
-    log.info("metrics job is summitted!!!");
 
     log.info("start read metadata from Pravega stream of flink job.");
-    //final StreamExecutionEnvironment newEnv = StreamExecutionEnvironment.getExecutionEnvironment();
     PravegaReadJob job = PravegaReadJob.getInstance();
     //job.readStream(env, parameters, "dataScope", "metaStream");*/
     log.info("!!!!!!!!!!!!! my person test case...");
@@ -47,7 +47,11 @@ public class PrometheusExampleJob {
 
     DataStream<PravegaReadJob.Person> adults = flintstones.filter((FilterFunction<PravegaReadJob.Person>) person -> person.age >= 18);
     adults.print();
-
+    Iterator<PravegaReadJob.Person> it = DataStreamUtils.collect(adults);
+    while(it.hasNext()) {
+      PravegaReadJob.Person one = it.next();
+      log.info("------- person name: {}, age: {}", one.name, one.age);
+    }
     env.execute(PrometheusExampleJob.class.getSimpleName());
     log.info("$$$$$$$$$$$$$ flink job is summited..");
   }
